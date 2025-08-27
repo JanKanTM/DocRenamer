2c4f4149be31adce3f4769f726bbf03acae135ec
Jan <95972736+JanKanTM@users.noreply.github.com>
2025-08-27 19:17:40 +0200

    changed the name of the output log file.

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,8 @@
 import os
 
 ### watch folder ###
-SCAN_FOLDER = "E:\\React\\DocRenamer\\test"
+SCAN_FOLDER = "path/to/your/folder"
 
 ### log-file ###
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-LOG_FILE_PATH = os.path.join(PROJECT_ROOT, 'logs', 'rename.log')
+LOG_FILE_PATH = os.path.join(PROJECT_ROOT, 'logs', 'main.log')
